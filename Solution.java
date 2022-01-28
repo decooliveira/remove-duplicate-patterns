20b4afe73baf9ade20ab4de3f2907f1c8a0aaaf1
Deco Oliveira <deco.oliveira@googlemail.com>
2022-01-28 12:03:30 -0300

    Including strings to test

--- Solution.java
+++ Solution.java
@@ -2,48 +2,20 @@ import java.util.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        String third = "abcabcabcdeco";
-        String one = "acdcdecd";
-        findSubstrings(third);
-        findSubstrings(one);
-    }
-
     public static Map<String, String> map = new HashMap<>();
 
-    public static String removeRepeatedSequences(String sequence, String substring) {
+    public static void main(String[] args) {
 
-        int position = sequence.indexOf(substring);
-        int len = substring.length();
-        int nextPosition = position + len;
+        String s = "abcabcabcdeco";
+        String s1 = "aaabbc";
+        String s2 = "daaca";
+        String s3 = "acdcdecd";
 
-        if (map.containsKey(substring)) {
-            int idx = sequence.indexOf(substring, nextPosition);
-
-            if (idx > 0) {
-                StringBuilder sb = new StringBuilder();
-
-                String firstHalf = sequence.substring(0, idx);
-                String secondHalf = sequence.substring(idx);
-
-                sb.append(firstHalf);
-
-                if (firstHalf.contains(substring) && firstHalf.endsWith(substring)) {
-                    secondHalf = secondHalf.replaceFirst(substring, "");
-                }
-
-                sb.append(secondHalf);
-                sequence = sb.toString();
-            }
-
-        } else {
-            map.put(substring, substring);
-        }
-
-        return sequence;
-
+        findSubstrings(s);
+        findSubstrings(s1);
+        findSubstrings(s2);
+        findSubstrings(s3);
     }
-
 
     public static void findSubstrings(String param) {
 
@@ -86,6 +58,39 @@ public class Solution {
         }
 
         print(param);
+
+    }
+
+    public static String removeRepeatedSequences(String sequence, String substring) {
+
+        int position = sequence.indexOf(substring);
+        int len = substring.length();
+        int nextPosition = position + len;
+
+        if (map.containsKey(substring)) {
+            int idx = sequence.indexOf(substring, nextPosition);
+
+            if (idx > 0) {
+                StringBuilder sb = new StringBuilder();
+
+                String firstHalf = sequence.substring(0, idx);
+                String secondHalf = sequence.substring(idx);
+
+                sb.append(firstHalf);
+
+                if (firstHalf.contains(substring) && firstHalf.endsWith(substring)) {
+                    secondHalf = secondHalf.replaceFirst(substring, "");
+                }
+
+                sb.append(secondHalf);
+                sequence = sb.toString();
+            }
+
+        } else {
+            map.put(substring, substring);
+        }
+
+        return sequence;
 
     }
 
